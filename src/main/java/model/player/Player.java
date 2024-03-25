13450de0049c19572e49234ba2a0a3f0e0613f1c
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-03-25 21:41:58 +0100

    updated Player with Goal reference

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -7,6 +7,7 @@ import model.chat.Chat;
 import model.deck.Deck;
 import model.deck.DeckBuffer;
 import model.game.Game;
+import model.goal.Goal;
 
 public class Player {
     private Game game;
@@ -15,6 +16,7 @@ public class Player {
     private Pawn playerPawn;
     private Hand playerHand;
     private Field playerField;
+    private Goal playerGoal;
     private Chat playerChat;
     public Game getGame() { return game; }
     public boolean getFirstPlayer() { return firstPlayer; }
@@ -38,10 +40,14 @@ public class Player {
         return playerHand.removeCard(card);
     }
     public boolean drawCard(Deck deck) {
+        if (deck == null)
+            return false;
         Card newCard = deck.draw();
         return playerHand.addCard(newCard);
     }
     public boolean drawCard(DeckBuffer deckBuffer) {
+        if (deckBuffer == null)
+            return false;
         Card newCard = deckBuffer.draw();
         return playerHand.addCard(newCard);
     }
