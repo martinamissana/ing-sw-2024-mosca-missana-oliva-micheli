282fe8f7bf7ec49e78a5d313e7796eebe13766d3
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-03-25 23:54:31 +0100

    added Coords (coordinates) class

---
+++ src/main/java/model/player/Coords.java
@@ -0,0 +1,16 @@
+package model.player;
+
+public class Coords {
+    private final int x;
+    private final int y;
+    public Coords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
