75335b8a0583761028ec50c70710af825be46a98
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-03-26 15:08:40 +0100

    renamed Player fields and removed Game reference

--- src/main/java/it/polimi/ingsw/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -6,49 +6,46 @@ import it.polimi.ingsw.model.card.StarterCard;
 import it.polimi.ingsw.model.chat.Chat;
 import it.polimi.ingsw.model.deck.Deck;
 import it.polimi.ingsw.model.deck.DeckBuffer;
-import it.polimi.ingsw.model.game.Game;
 import it.polimi.ingsw.model.goal.Goal;
 
 public class Player {
-    private Game game;
-    private boolean firstPlayer;
+    private boolean isFirstPlayer;
     private String nickname;
-    private Pawn playerPawn;
-    private Hand playerHand;
-    private Field playerField;
-    private Goal playerGoal;
-    private Chat playerChat;
-    public Game getGame() { return game; }
-    public boolean getFirstPlayer() { return firstPlayer; }
+    private Pawn pawn;
+    private Hand hand;
+    private Field field;
+    private Goal privateGoal;
+    private Chat chat;
+    public boolean getFirstPlayer() { return isFirstPlayer; }
     public String getNickname() { return nickname; }
-    public Pawn getPlayerColor() { return playerPawn; }
-    public Hand getPlayerHand() { return playerHand; }
-    public Field getPlayerField() { return playerField; }
-    public Chat getPlayerChat() { return playerChat; }
+    public Pawn getPlayerColor() { return pawn; }
+    public Hand getPlayerHand() { return hand; }
+    public Field getPlayerField() { return field; }
+    public Chat getPlayerChat() { return chat; }
 
     public boolean playCard() {
-        StarterCard card = (StarterCard) playerHand.getCard(0);
-        playerField.addCard(card);
-        playerHand.removeCard(card);
+        StarterCard card = (StarterCard) hand.getCard(0);
+        field.addCard(card);
+        hand.removeCard(card);
         return true;
     }
     public boolean playCard(int handPos, Coords coords) {
-        ResourceCard card = (ResourceCard) playerHand.getCard(handPos);
-        boolean cardWasAdded = playerField.addCard((ResourceCard) card, coords);
+        ResourceCard card = (ResourceCard) hand.getCard(handPos);
+        boolean cardWasAdded = field.addCard((ResourceCard) card, coords);
         if (!cardWasAdded)
             return false;
-        return playerHand.removeCard(card);
+        return hand.removeCard(card);
     }
     public boolean drawCard(Deck deck) {
         if (deck == null)
             return false;
         Card newCard = deck.draw();
-        return playerHand.addCard(newCard);
+        return hand.addCard(newCard);
     }
     public boolean drawCard(DeckBuffer deckBuffer) {
         if (deckBuffer == null)
             return false;
         Card newCard = deckBuffer.draw();
-        return playerHand.addCard(newCard);
+        return hand.addCard(newCard);
     }
 }
