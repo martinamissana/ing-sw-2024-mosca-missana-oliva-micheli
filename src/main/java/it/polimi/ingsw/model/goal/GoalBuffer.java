2690349cbcb294d3a7151e0290de0a9a80b550f7
Martina Missana <martina.missana@mail.polimi.it>
2024-03-26 12:28:37 +0100

    added method getgoal

--- src/main/java/it/polimi/ingsw/model/goal/GoalBuffer.java
+++ src/main/java/it/polimi/ingsw/model/goal/GoalBuffer.java
@@ -1,6 +1,19 @@
 package it.polimi.ingsw.model.goal;
+import it.polimi.ingsw.model.player.Player;
 
+import java.util.*;
 
 public class GoalBuffer {
+    private static List<Goal> goalDeck= new ArrayList<>();
 
+    public GoalBuffer(List<Goal> goalDeck) {
+        GoalBuffer.goalDeck =goalDeck;
+    }
+
+    public Goal getgoal(){
+        Collections.shuffle(goalDeck);
+        Goal goal=goalDeck.get(0);
+        goalDeck.remove(0);
+        return goal;
+    }
 }
