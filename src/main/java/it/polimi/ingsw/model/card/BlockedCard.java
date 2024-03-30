a4263eee8a4cf2c7e243a0d351da81264765f378
Silvia <silvia.mosca@mail.polimi.it>
2024-03-30 12:04:21 +0100

    BlockedCard edited

--- src/main/java/it/polimi/ingsw/model/card/BlockedCard.java
+++ src/main/java/it/polimi/ingsw/model/card/BlockedCard.java
@@ -2,9 +2,17 @@ package it.polimi.ingsw.model.card;
 
 import java.util.HashMap;
 
+/**
+ * Class BlockedCard
+ * instantiated in the field where it is not possible to place other cards due to the absence of one or more corner in the adjacent spots
+ */
 public class BlockedCard extends Card {
 
-    public BlockedCard(int cardID, CardSide side, HashMap<CornerType, Corner> frontCorner, HashMap<CornerType, Corner> backCorner) {
-        super(cardID, side, frontCorner, backCorner);
+    /**
+     * Class constructor
+     * sets all the attributes to default values as they are not relevant
+     */
+    public BlockedCard() {
+        super(-1, CardSide.FRONT, null, null);
     }
 }
