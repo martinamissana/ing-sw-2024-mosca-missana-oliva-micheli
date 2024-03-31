6296b5828cd9f4c0d347d88a2fc07005564b5f7c
Silvia <silvia.mosca@mail.polimi.it>
2024-03-31 14:19:40 +0200

    updated Chat and Message classes

--- src/main/java/it/polimi/ingsw/model/chat/Chat.java
+++ src/main/java/it/polimi/ingsw/model/chat/Chat.java
@@ -7,34 +7,98 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class Chat
+ * each Player has an associated chat with a list of sent messages and a list of received messages
+ */
 public class Chat {
-    private final Player player;
-    private HashMap<Player, List<Message>> directMessages;
-    private ArrayList<Message> globalMessages;
-    private Game game;
+    private ArrayList<Message> sentMessages;
+    private ArrayList<Message> receivedMessages;
 
+    /**
+     * Class constructor
+     * @param sentMessages
+     * @param receivedMessages
+     */
+    public Chat(ArrayList<Message> sentMessages,ArrayList<Message> receivedMessages){
+        this.sentMessages = sentMessages;
+        this.receivedMessages = receivedMessages;
+    }
 
-    public Chat(Player player){
-        for (Map.Entry<Integer, Player> entry : game.getPlayers().entrySet()) {
-            Player K = entry.getValue();
-            directMessages.put(K, null);
+    /**
+     * gets the received messages
+     * @return receivedMessages
+     */
+    public ArrayList<Message> getReceivedMessages() {
+        return receivedMessages;
+    }
+    /**
+     * gets the sent messages
+     * @return sentMessages
+     */
+    public ArrayList<Message> getSentMessages() {
+        return sentMessages;
+    }
+
+    /**
+     * gets the messages received from a specified player
+     * @param sender
+     * @return list of messages from sender
+     */
+    public ArrayList<Message> getReceivedMessagesFromPlayer(Player sender) {
+        int i;
+        ArrayList<Message> list= new ArrayList<Message>();
+        for(i=0;i<=receivedMessages.size();i++){
+            if(receivedMessages.get(i).getSender()==sender){
+                list.add(receivedMessages.get(i));
+            }
         }
-        this.player=player;
+        return list;
     }
 
-    public HashMap<Player, List<Message>> getDirectMessages() {
-        return directMessages;
-    }
-
-    public ArrayList<Message> getGlobalMessages() {
-        return globalMessages;
-    }
-
-    public void addMessage(Player sender, Message message, boolean global){
-        if(global==false){
-            directMessages.get(sender).add(message);
-        } else {
-            globalMessages.add(message);
+    /**
+     * gets the messages sent to a specified player
+     * @param receiver
+     * @return list of messages to receiver
+     */
+    public ArrayList<Message> getSentMessagesToPlayer(Player receiver) {
+        int i;
+        ArrayList<Message> list= new ArrayList<Message>();
+        for(i=0;i<=sentMessages.size();i++){
+            if(sentMessages.get(i).getSender()==receiver){
+                list.add(sentMessages.get(i));
+            }
         }
+        return list;
+    }
+
+    /**
+     * gets the messages received in the global chat
+     * @return list of global received message
+     */
+    public ArrayList<Message> getGlobalReceivedMessages() {
+        int i;
+        ArrayList<Message> list= new ArrayList<Message>();
+        for(i=0;i<=receivedMessages.size();i++){
+            if(receivedMessages.get(i).isGlobal()){
+                list.add(receivedMessages.get(i));
+            }
+        }
+        return list;
+    }
+
+    /**
+     * gets the messages sent in the global chat
+     * @return list of global sent messages
+     */
+    public ArrayList<Message> getGlobalSentMessages() {
+        int i;
+        ArrayList<Message> list= new ArrayList<Message>();
+        for(i=0;i<=sentMessages.size();i++){
+            if(sentMessages.get(i).isGlobal()){
+                list.add(sentMessages.get(i));
+            }
+        }
+        return list;
     }
 }
--- src/main/java/it/polimi/ingsw/model/chat/Message.java
+++ src/main/java/it/polimi/ingsw/model/chat/Message.java
@@ -2,38 +2,74 @@ package it.polimi.ingsw.model.chat;
 
 import it.polimi.ingsw.model.player.Player;
 
-import java.util.HashMap;
-
+/**
+ * Class Message
+ * contains all the attributes associated to a message and the method to send it
+ */
 public class Message {
     private final String text;
     private final Player sender;
+    private final Player receiver;
     private final boolean isGlobal;
 
-    public Message(String text, Player sender,boolean global){
-        this.sender=sender;
-        this.text=text;
-        this.isGlobal=global;
+    /**
+     * CLass constructor
+     * @param text
+     * @param sender
+     * @param receiver
+     * @param global
+     */
+    public Message(String text, Player sender, Player receiver, boolean global) {
+        this.sender = sender;
+        this.text = text;
+        this.receiver = receiver; //null if global
+        this.isGlobal = global;
     }
 
+    /**
+     * gets the text
+     * @return message text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * gets the sender of the message
+     * @return  message sender
+     */
     public Player getSender() {
         return sender;
     }
 
+    /**
+     * gets the receiver of the message
+     * @return  message receiver
+     */
+    public Player getReceiver() {
+        return receiver;
+    }
+
+    /**
+     * tells if a message is global
+     * @return message isGlobal
+     */
     public boolean isGlobal() {
         return isGlobal;
     }
 
-    public void sendMessage(Player player){
-        if(player==null){
-            for( HashMap.Entry<Integer, Player> entry: player.getGame().getPlayers().entrySet()){
-                player.getPlayerChat().addMessage(this.sender,this,true);
-            }
-        } else{
-            player.getPlayerChat().addMessage(this.sender,this,false);
+
+    /**
+     * sends the message  either to the specified receiver, or to every player in the global chat,
+     * adds the message in the list of sent messages of the sender too
+     */
+    public void send() {
+        if(!isGlobal) {
+            sender.getChat().getSentMessages().add(this);
+            receiver.getChat().getReceivedMessages().add(this);
+        }
+        else {
+            //che fare se global?
         }
     }
 }
