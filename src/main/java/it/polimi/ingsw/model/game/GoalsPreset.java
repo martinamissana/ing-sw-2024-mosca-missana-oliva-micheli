d9b903aea3de8cc74cd1d9b57958206ecf095ba9
Martina Missana <martina.missana@mail.polimi.it>
2024-04-02 18:07:13 +0200

    added builder and deserializer

---
+++ src/main/java/it/polimi/ingsw/model/game/GoalsPreset.java
@@ -0,0 +1,99 @@
+package it.polimi.ingsw.model.game;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.polimi.ingsw.model.commonItem.ItemBox;
+import it.polimi.ingsw.model.goal.*;
+import it.polimi.ingsw.model.serialization.ItemboxDeserializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Class GoalsPreset
+ * defines the methods to obtain all type of goals in Codex Naturalis
+ */
+public class GoalsPreset {
+
+    /**
+     * gets all the diagonal goals in a list
+     * @return ArrayList<DiagonalGoal>
+     * @throws IOException
+     */
+    public static ArrayList<DiagonalGoal> getDiagonalGoals() throws IOException{
+
+        InputStream inputStream = CardsPreset.class.getClassLoader().getResourceAsStream("diagonal_goals.json");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder jsonBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            jsonBuilder.append(line);
+        }
+        String json = jsonBuilder.toString();
+
+        GsonBuilder builder = new GsonBuilder();
+
+        Gson gson = builder.create();
+
+        DiagonalGoal[] arrayGoal = gson.fromJson(json, DiagonalGoal[].class);
+
+        return new ArrayList<>(Arrays.asList(arrayGoal));
+    }
+
+    /**
+     * gets all the L_shape goals in a list
+     * @return ArrayList<L_ShapeGoal>
+     * @throws IOException
+     */
+    public static ArrayList<L_ShapeGoal> getLShapeGoals() throws IOException{
+
+        InputStream inputStream = CardsPreset.class.getClassLoader().getResourceAsStream("L_shape_goals.json");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder jsonBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            jsonBuilder.append(line);
+        }
+        String json = jsonBuilder.toString();
+
+        GsonBuilder builder = new GsonBuilder();
+
+        Gson gson = builder.create();
+
+        L_ShapeGoal[] arrayGoal = gson.fromJson(json, L_ShapeGoal[].class);
+
+        return new ArrayList<>(Arrays.asList(arrayGoal));
+    }
+
+    /**
+     * gets all the resource goals in a list
+     * @return ArrayList<ResourceGoal>
+     * @throws IOException
+     */
+    public static ArrayList<ResourceGoal> getResourceGoals() throws IOException{
+
+        InputStream inputStream = CardsPreset.class.getClassLoader().getResourceAsStream("resource_goals.json");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder jsonBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            jsonBuilder.append(line);
+        }
+        String json = jsonBuilder.toString();
+
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(ItemBox.class, new ItemboxDeserializer());
+        Gson gson = builder.create();
+
+        ResourceGoal[] arrayGoal = gson.fromJson(json, ResourceGoal[].class);
+
+        return new ArrayList<>(Arrays.asList(arrayGoal));
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/serialization/ItemboxDeserializer.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.model.serialization;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import it.polimi.ingsw.model.commonItem.ItemBox;
+import it.polimi.ingsw.model.commonItem.Kingdom;
+import it.polimi.ingsw.model.commonItem.Resource;
+
+import java.lang.reflect.Type;
+
+/**
+ * Class ItemBoxDeserializer
+ * used to help deserialize the json files
+ */
+public class ItemboxDeserializer implements JsonDeserializer<ItemBox>{
+    /**
+     * method called in GoalsPreset to help with the deserialization and the instantiation of the Arraylist of resources in Resource Goals
+     * @param jsonElement
+     * @param type
+     * @param jsonDeserializationContext
+     * @return an item (Kingdom or Resource, depending on what type of Itembox it is)
+     * @throws JsonParseException
+     */
+    @Override
+    public ItemBox deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        String value = jsonElement.getAsString();
+        ItemBox item;
+        if(value.equals("ANIMAL") ||
+                value.equals("FUNGI") ||
+                value.equals("INSECT") ||
+                value.equals("PLANT"))
+            item = jsonDeserializationContext.deserialize(jsonElement, Kingdom.class);
+        else if(value.equals("QUILL") ||
+                value.equals("INKWELL") ||
+                value.equals("MANUSCRIPT"))
+            item = jsonDeserializationContext.deserialize(jsonElement, Resource.class);
+        else
+            throw new JsonParseException("Item not valid: " + value);
+        return item;
+    }
+}
