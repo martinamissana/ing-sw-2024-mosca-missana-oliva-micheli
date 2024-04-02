9fc0b2b8d2b4dc50ab6efefa5dbb714573500c2d
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-04-02 15:05:20 +0200

    added Player class constructor

--- src/main/java/it/polimi/ingsw/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -9,20 +9,101 @@ import it.polimi.ingsw.model.deck.DeckBuffer;
 import it.polimi.ingsw.model.goal.Goal;
 
 public class Player {
+    private final String nickname;
+    private final Hand hand;
+    private final Field field;
+    private final Chat chat;
     private boolean goesFirst;
-    private String nickname;
-    private Pawn pawn;
-    private Hand hand;
-    private Field field;
     private Goal privateGoal;
-    private Chat chat;
-    public boolean getGoesFirst() { return goesFirst; }
+    private Pawn pawn;
+
+    /**
+     * Class constructor
+     * @param nickname
+     */
+    public Player(String nickname) {
+        this.nickname = nickname;
+        this.hand = new Hand();
+        this.field = new Field();
+        this.chat = new Chat();
+        this.goesFirst = false;
+        this.privateGoal = null;
+        this.pawn = null;
+    }
+
+    /**
+     * Class constructor
+     * @param goesFirst
+     * @param nickname
+     * @param pawn
+     */
+    // might not be needed
+    public Player(String nickname, boolean goesFirst, Pawn pawn) {
+        this.nickname = nickname;
+        this.hand = new Hand();
+        this.field = new Field();
+        this.chat = new Chat();
+        this.goesFirst = goesFirst;
+        this.privateGoal = null;
+        this.pawn = pawn;
+    }
+
+    /**
+     * sets the player to be the one to go first
+     * @param goesFirst
+     */
+    public void setGoesFirst(boolean goesFirst) { this.goesFirst = goesFirst; }
+
+    /**
+     * sets the player's private goal
+     * @param goal
+     */
+    public void setPrivateGoal(Goal goal) { this.privateGoal = goal; }
+
+    /**
+     * sets the player's pawn
+     * @param pawn
+     */
+    public void setPawn(Pawn pawn) { this.pawn = pawn; }
+
+    /**
+     * returns the player's nickname
+     * @return String
+     */
     public String getNickname() { return nickname; }
-    public Pawn getColor() { return pawn; }
+
+    /**
+     * returns the player's hand
+     * @return Hand
+     */
     public Hand getHand() { return hand; }
+
+    /**
+     * returns the player's field
+     * @return Field
+     */
     public Field getField() { return field; }
+
+    /**
+     * returns the player's chat
+     * @return Chat
+     */
     public Chat getChat() { return chat; }
 
+    /**
+     * returns whether the player goes first or not
+     * @return boolean
+     */
+    public boolean getGoesFirst() { return goesFirst; }
+
+    /**
+     * returns the player's pawn
+     * @return Pawn
+     */
+    public Pawn getPawn() { return pawn; }
+
+    // this should all be in the controller side of things
+    /*
     public boolean playCard() {
         StarterCard card = (StarterCard) hand.getCard(0);
         field.addCard(card);
@@ -31,9 +112,8 @@ public class Player {
     }
     public boolean playCard(int handPos, Coords coords) {
         ResourceCard card = (ResourceCard) hand.getCard(handPos);
-        boolean cardWasAdded = field.addCard((ResourceCard) card, coords);
-        if (!cardWasAdded)
-            return false;
+        // very temporary
+        int points = field.addCard((ResourceCard) card, coords);
         return hand.removeCard(card);
     }
     public boolean drawCard(Deck deck) {
@@ -47,5 +127,5 @@ public class Player {
             return false;
         Card newCard = deckBuffer.draw();
         return hand.addCard(newCard);
-    }
+    }*/
 }
