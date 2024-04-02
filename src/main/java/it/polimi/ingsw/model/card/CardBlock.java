e35c7ca10f371508db466bd00b9e13bf69c753a3
Silvia <silvia.mosca@mail.polimi.it>
2024-04-02 16:17:51 +0200

    changed name for class BlockedCard

--- src/main/java/it/polimi/ingsw/model/card/CardBlock.java
+++ src/main/java/it/polimi/ingsw/model/card/CardBlock.java
@@ -1,18 +1,16 @@
 package it.polimi.ingsw.model.card;
 
-import java.util.HashMap;
-
 /**
  * Class BlockedCard
  * instantiated in the field where it is not possible to place other cards due to the absence of one or more corner in the adjacent spots
  */
-public class BlockedCard extends Card {
+public class CardBlock extends Card {
 
     /**
      * Class constructor
      * sets all the attributes to default values as they are not relevant
      */
-    public BlockedCard() {
+    public CardBlock() {
         super(-1, CardSide.FRONT, null, null);
     }
 }
