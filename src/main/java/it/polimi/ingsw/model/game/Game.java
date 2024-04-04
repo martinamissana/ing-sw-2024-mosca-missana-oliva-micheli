9dbfa0ae8169fedfa63461501b5bfaa3c2ea1f93
Martina Missana <martina.missana@mail.polimi.it>
2024-04-04 14:33:04 +0200

    merged scoreboard and game classes

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -5,19 +5,50 @@ import it.polimi.ingsw.model.player.Player;
 import java.util.HashMap;
 
 public class Game {
-   private int numOfPlayer;
-   private HashMap<Integer, Player> players;
 
-    public Game(int numOfPlayer, HashMap<Integer, Player> players) {
+    private final int gameID;
+   private final int numOfPlayer;
+   private final HashMap<Player, Integer> players;
+   private int whosTurn;
+   private HashMap<Player,Integer> scoreboard;
+
+    public Game(int gameID,int numOfPlayer, HashMap<Player, Integer> players,HashMap<Player,Integer> scoreboard) {
         this.numOfPlayer = numOfPlayer;
         this.players = players;
+        this.gameID=gameID;
+        this.whosTurn=0;
+        this.scoreboard=scoreboard;
+    }
+
+    public void setWhosTurn(int whosTurn) {
+        this.whosTurn = whosTurn;
+    }
+
+    public int getWhosTurn() {
+        return whosTurn;
+    }
+
+    public int getGameID() {
+        return gameID;
     }
 
     public int getNumOfPlayer() {
         return numOfPlayer;
     }
 
-    public HashMap<Integer, Player> getPlayers() {
+    public HashMap<Player, Integer> getPlayers() {
         return players;
+    }
+
+    public void save(){
+
+    }
+
+    public void setPlayePoints(Player player,int points) {
+        this.scoreboard.put(player,points);
+    }
+
+    public int getScoreboard(Player player) {
+        return scoreboard.get(player);
     }
 }
