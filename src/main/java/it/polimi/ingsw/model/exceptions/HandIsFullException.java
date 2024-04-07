8e8c9e15c85ce9d57dbe926fdd4d97629f159151
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-04-07 23:30:09 +0200

    added exceptions to Field and Hand classes

---
+++ src/main/java/it/polimi/ingsw/model/exceptions/HandIsFullException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.model.exceptions;
+
+public class HandIsFullException extends Exception {
+    public HandIsFullException() { super(); }
+}
---
+++ src/main/java/it/polimi/ingsw/model/exceptions/RequirementsNotSatisfiedException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model.exceptions;
+
+/**
+ * thrown when trying to set a GoldenCard that doesn't have its requirements met
+ */
+public class RequirementsNotSatisfiedException extends IllegalMoveException {
+    public RequirementsNotSatisfiedException() { super(); }
+}
