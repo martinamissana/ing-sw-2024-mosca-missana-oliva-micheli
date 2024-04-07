a938978f9a6aa2a5c39ce3cd07afffeaef6f966f
Silvia <silvia.mosca@mail.polimi.it>
2024-04-07 11:22:13 +0200

    added methods in Controller

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.game.GameHandler;
+
 public class Controller {
+    GameHandler gh;
+
+    public Controller(GameHandler gh) {
+        this.gh = gh;
+    }
+
+    public GameHandler getGh() {
+        return gh;
+    }
 }
