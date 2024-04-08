52a7143a4f0f23683051f701b6a0ea75210b753a
Silvia <silvia.mosca@mail.polimi.it>
2024-04-08 15:38:09 +0200

    updated GameHandler

--- src/main/java/it/polimi/ingsw/model/game/GameHandler.java
+++ src/main/java/it/polimi/ingsw/model/game/GameHandler.java
@@ -1,27 +1,24 @@
 package it.polimi.ingsw.model.game;
 
-import it.polimi.ingsw.model.card.GoldenCard;
-import it.polimi.ingsw.model.chat.Chat;
-import it.polimi.ingsw.model.commonItem.Kingdom;
 import it.polimi.ingsw.model.player.Player;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 public class GameHandler implements Serializable {
 
-    private ArrayList<Game> activeGames;
+    private HashMap<Integer,Game> activeGames;
     private ArrayList<Lobby> lobbies;
+    private static int numOfGames=0;
 
     public GameHandler() {
-        this.activeGames = new ArrayList<Game>();
+        this.activeGames = new HashMap<>();
         this.lobbies = new ArrayList<Lobby>();
     }
 
-    public ArrayList<Game> getActiveGames() {
+    public HashMap<Integer, Game> getActiveGames() {
         return activeGames;
     }
 
@@ -29,13 +26,19 @@ public class GameHandler implements Serializable {
         return lobbies;
     }
 
+    public Game getGame(int ID){
+        return activeGames.get(ID);
+    }
+
+    public static int getNumOfGames() {
+        return numOfGames;
+    }
+
+
     public void createLobby(int numOfPlayers){
         lobbies.add(new Lobby(numOfPlayers));
     }
-    public Game loadGame() {
-        //need to deserialize the file
-        return null;
-    }
+
 
     public void createGame(Lobby lobby){
         HashMap<Integer,Player> playerIntegerHashMap=new HashMap<>();
@@ -50,8 +53,11 @@ public class GameHandler implements Serializable {
         for(Integer key : playerIntegerHashMap.keySet()){
             scoreboard.put(playerIntegerHashMap.get(key),0);
         }
-        activeGames.add(new Game(activeGames.size(),lobby.getNumOfPlayers(),playerIntegerHashMap,scoreboard));
+        activeGames.put(numOfGames,new Game(numOfGames,lobby.getNumOfPlayers(),playerIntegerHashMap,scoreboard));
+        numOfGames++;
     }
+
+
     public void save() throws IOException {
         FileOutputStream fileOutputStream = new FileOutputStream("./data.ser");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
