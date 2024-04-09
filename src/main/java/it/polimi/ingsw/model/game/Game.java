55744cad38054c697563e66089f261bef922f821
Martina Missana <martina.missana@mail.polimi.it>
2024-04-09 10:45:41 +0200

    added javadoc and decks in game

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -1,80 +1,190 @@
 package it.polimi.ingsw.model.game;
 
+import it.polimi.ingsw.model.deck.Deck;
+import it.polimi.ingsw.model.deck.DeckBuffer;
+import it.polimi.ingsw.model.deck.DeckType;
 import it.polimi.ingsw.model.goal.Goal;
 import it.polimi.ingsw.model.player.Player;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 
+/**
+ * Game class, it represents a single instance of a match
+ */
 public class Game implements Serializable {
 
     private final int gameID;
-    private final int numOfPlayer;
+    private final int numOfPlayers;
     private final HashMap<Integer,Player> players;
     private int whoseTurn;
     private final HashMap<Player,Integer> scoreboard;
     private Goal commonGoal1;
     private Goal commonGoal2;
+    private final Deck ResourceDeck=new Deck(DeckType.RESOURCE);
+    private final Deck GoldenDeck=new Deck(DeckType.GOLDEN);
+    private final HashMap<String,DeckBuffer> deckBuffers = new HashMap<>();
 
-    public Game(int gameID, int numOfPlayer, HashMap<Integer, Player> players, HashMap<Player,Integer> scoreboard) {
-        this.numOfPlayer = numOfPlayer;
+
+    /**
+     * Class Constructor
+     * @param gameID - identifies univocally the game
+     * @param numOfPlayers - number of players of the game
+     * @param players - HashMap<Integer, Player> maps PlayerID to Player
+     * @param scoreboard - HashMap<Player,Integer> maps the player to his points
+     * @throws IOException
+     */
+    public Game(int gameID, int numOfPlayers, HashMap<Integer, Player> players, HashMap<Player,Integer> scoreboard) throws IOException {
+        this.numOfPlayers = numOfPlayers;
         this.players = players;
         this.gameID=gameID;
         this.commonGoal1 = null;
         this.commonGoal2 = null;
         this.whoseTurn=0;
         this.scoreboard=scoreboard;
+        this.deckBuffers.put("gold1",new DeckBuffer(this.GoldenDeck));
+        this.deckBuffers.put("gold2",new DeckBuffer(this.GoldenDeck));
+        this.deckBuffers.put("res1",new DeckBuffer(this.ResourceDeck));
+        this.deckBuffers.put("res2",new DeckBuffer(this.ResourceDeck));
     }
 
+    /**
+     * getter
+     * @param type - specifies the type of the deckbuffer needed (gold1,gold2,res1,res2)
+     * @return Deckbuffer
+     */
+    public DeckBuffer getDeckBuffer(String type){
+        return deckBuffers.get(type);
+    }
+
+    /**
+     * Setter
+     * @param whoseTurn - int, indicates the PlayerID of who is playing at the moment
+     */
     public void setWhoseTurn(int whoseTurn) {
         this.whoseTurn = whoseTurn;
     }
 
+    /**
+     * getter
+     * @return int - PlayerID of who is playing at the moment
+     */
     public int getWhoseTurn() {
         return whoseTurn;
     }
 
+    /**
+     * getter
+     * @return int- GameID
+     */
     public int getGameID() {
         return gameID;
     }
 
+    /**
+     * getter
+     * @return int- NumOfPlayer
+     */
     public int getNumOfPlayer() {
-        return numOfPlayer;
+        return numOfPlayers;
     }
 
+    /**
+     * getter
+     * @return HashMap<Integer,Player> - players
+     */
     public HashMap<Integer,Player> getPlayers() {
         return players;
     }
 
-    public void save(){
-
-    }
-
+    /**
+     * getter
+     * @return Goal - one of the two common goals
+     */
     public Goal getCommonGoal1() {
         return commonGoal1;
     }
 
+    /**
+     * getter
+     * @return Goal - one of the two common goals
+     */
     public Goal getCommonGoal2() {
         return commonGoal2;
     }
 
+    /**
+     * setter
+     * @param commonGoal1 - Goal
+     */
     public void setCommonGoal1(Goal commonGoal1) {
         this.commonGoal1 = commonGoal1;
     }
 
+    /**
+     * setter
+     * @param commonGoal2 - Goal
+     */
     public void setCommonGoal2(Goal commonGoal2) {
         this.commonGoal2 = commonGoal2;
     }
+
+    /**
+     * increments the points of Player player
+     * @param player
+     * @param points
+     */
     public void addToScore(Player player,int points){
         scoreboard.put(player,scoreboard.get(player)+points);
     }
+
+    /**
+     * sets the points of Player player
+     * @param player
+     * @param points
+     */
     public void setPlayerScore(Player player,int points) {
         this.scoreboard.put(player,points);
     }
+
+    /**
+     * gets the points of the player
+     * @param player
+     * @return int
+     */
     public int getPlayerScore(Player player) {
         return scoreboard.get(player);
     }
+
+    /**
+     * @return Player - the player who is playing at the moment
+     */
     public Player getCurrPlayer(){
         return players.get(whoseTurn);
+    }
+
+    /**
+     * getter
+     * @return HashMap<Player,Integer> - the entire scoreboard
+     */
+    public HashMap<Player, Integer> getScoreboard() {
+        return scoreboard;
+    }
+
+    /**
+     * getter
+     * @return Deck - ResourceDeck of the game
+     */
+    public Deck getResourceDeck() {
+        return ResourceDeck;
+    }
+
+    /**
+     * getter
+     * @return Deck - GoldenDeck of the game
+     */
+    public Deck getGoldenDeck() {
+        return GoldenDeck;
     }
 }
