5e44256ab9ab78cabef02021049929391a170ac5
Martina Missana <martina.missana@mail.polimi.it>
2024-04-14 11:12:09 +0200

    added javadoc and changed players to arraylist

--- src/main/java/it/polimi/ingsw/model/game/Lobby.java
+++ src/main/java/it/polimi/ingsw/model/game/Lobby.java
@@ -2,44 +2,76 @@ package it.polimi.ingsw.model.game;
 
 import it.polimi.ingsw.model.exceptions.FullLobbyException;
 import it.polimi.ingsw.model.exceptions.NicknameAlreadyTakenException;
+import it.polimi.ingsw.model.player.Player;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
 
+/**
+ * Lobby class, it represents a waiting room that players can join. When the number of players required is reached, the game is created
+ */
 public class Lobby implements Serializable {
 
-    private HashSet<String> players;
+    private ArrayList<Player> players;
     private int numOfPlayers;
 
+    /**
+     * Constructor
+     * @param numOfPlayers - number of players required
+     */
     public Lobby(int numOfPlayers){
         this.numOfPlayers=numOfPlayers;
-        this.players=new HashSet<String>();
+        this.players=new ArrayList<>();
     }
 
+    /**
+     * getter
+     * @return int - numOfPlayers
+     */
     public int getNumOfPlayers() {
         return numOfPlayers;
     }
 
-    public HashSet<String> getPlayers() {
+    /**
+     * getter
+     * @return ArrayList<Player> - players
+     */
+    public ArrayList<Player> getPlayers() {
         return players;
     }
 
-    public void addPlayer(String player) throws FullLobbyException, NicknameAlreadyTakenException {
-            if (players.size() == numOfPlayers) {
-                throw new FullLobbyException();
-            }
-            if (players.contains(player)) {
-                throw new NicknameAlreadyTakenException();
-            }
+    /**
+     * method to add players to the lobby
+     * @param  player - player to add to the lobby
+     * @throws FullLobbyException - the lobby is full
+     * @throws NicknameAlreadyTakenException - someone in the lobby has the same nickname
+     */
+    public void addPlayer(Player player) throws FullLobbyException, NicknameAlreadyTakenException {
+        if (lobbyFull()) {
+            throw new FullLobbyException();
+        }
+        for (Player p: players){
+            if(p.getNickname().equals(player.getNickname()))
+               throw new NicknameAlreadyTakenException();
+        }
         players.add(player);
     }
 
-    public void removePlayer(String player){
+    /**
+     * method to remove players from the lobby
+     * @param player
+     */
+    public void removePlayer(Player player){
         players.remove(player);
 
     }
 
-    public boolean lobbyFull(){
+    /**
+     * method that returns true if the lobby is full
+     * @return boolean
+     */
+    private boolean lobbyFull(){
         return players.size() >= numOfPlayers;
     }
 }
