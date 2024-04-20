eff10b7b88c6ff597ea8b6d3d218e7ddabc90bdd
Silvia <silvia.mosca@mail.polimi.it>
2024-04-20 10:33:12 +0200

    installed javaFX

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module PSP001 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
     requires com.google.gson;
 
     opens it.polimi.ingsw.model.card;
