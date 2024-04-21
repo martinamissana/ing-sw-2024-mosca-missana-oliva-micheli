a69735312d4b5c747becc1ba68ed3551bc672b1d
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-04-21 11:12:49 +0200

    GameController redesign

---
+++ src/main/java/it/polimi/ingsw/controller/exceptions/NotYourTurnException.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.controller.exceptions;
+
+import java.io.Serializable;
+
+/**
+ * thrown when a player tries to perform an action when it's not their turn
+ */
+public class NotYourTurnException extends Exception implements Serializable {
+    public NotYourTurnException() { super(); }
+}
