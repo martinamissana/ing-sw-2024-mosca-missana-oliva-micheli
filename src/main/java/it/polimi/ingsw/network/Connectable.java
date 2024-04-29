4afc2dd136b4c915dacbd01ca7c354ed47b1236d
Martina Missana <martina.missana@mail.polimi.it>
2024-04-29 15:18:50 +0200

    modified Connectable interface

--- src/main/java/it/polimi/ingsw/network/Connectable.java
+++ src/main/java/it/polimi/ingsw/network/Connectable.java
@@ -1,12 +1,12 @@
 package it.polimi.ingsw.network;
 
-import it.polimi.ingsw.model.chat.Message;
+import it.polimi.ingsw.network.message.NetMessage;
 import it.polimi.ingsw.network.exceptions.ConnectionException;
 
 import java.io.IOException;
 
 public interface Connectable {
-    public void send(Message message) throws IOException, ConnectionException;
-    public Message receive() throws InterruptedException, ConnectionException;
+    public void send(NetMessage message) throws IOException, ConnectionException;
+    public NetMessage receive() throws InterruptedException, ConnectionException;
     public void disconnect();
 }
