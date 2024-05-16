62743b127e4401c79a548ce355a6408950436ed5
Silvia <silvia.mosca@mail.polimi.it>
2024-05-16 17:23:32 +0200

    edit: client tcp logic

--- src/main/java/it/polimi/ingsw/network/Client.java
+++ src/main/java/it/polimi/ingsw/network/Client.java
@@ -9,13 +9,11 @@ import it.polimi.ingsw.view.TCPView;
 import it.polimi.ingsw.view.View;
 
 import java.io.IOException;
-import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.exit;
 
@@ -40,25 +38,12 @@ public class Client {
                 }).start();
 
                 new Thread(() -> {
-                    try {
-                        do{
-                            client.heartbeat();
-                            TimeUnit.SECONDS.sleep(3);
-                        }while(!client.getSocket().isClosed());
-                    } catch (IOException | ClassNotFoundException | InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).start();
-
-                new Thread(() -> {
                     CLI cli = new CLI(client);
                     cli.run();
                 }).start();
 
             } catch (IOException e) {
                 System.err.println(e.getMessage());
-            } catch (NicknameAlreadyTakenException e) {
-                throw new RuntimeException(e);
             }
         } else if (choice.equalsIgnoreCase("RMI")) {
             Registry registry = LocateRegistry.getRegistry();
