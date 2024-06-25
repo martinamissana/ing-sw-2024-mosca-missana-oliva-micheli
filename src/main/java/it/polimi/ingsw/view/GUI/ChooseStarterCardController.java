bb2e556c820f1ac1d4fdaf2192af5beda40fab03
Antonio Oliva <160790965+AntoniooOliva@users.noreply.github.com>
2024-06-25 17:29:34 +0200

    ChooseStarterCard: Modified card side selection

--- src/main/java/it/polimi/ingsw/view/GUI/ChooseStarterCardController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/ChooseStarterCardController.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw.view.GUI;
 import it.polimi.ingsw.controller.exceptions.UnexistentUserException;
 import it.polimi.ingsw.controller.exceptions.WrongGamePhaseException;
 import it.polimi.ingsw.model.card.CardSide;
+import it.polimi.ingsw.model.card.StarterCard;
 import it.polimi.ingsw.model.exceptions.EmptyDeckException;
 import it.polimi.ingsw.model.exceptions.GameDoesNotExistException;
 import it.polimi.ingsw.model.exceptions.HandIsFullException;
-import it.polimi.ingsw.view.View;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
@@ -14,25 +14,28 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class ChooseStarterCardController {
-
-    private View view;
+    private final ViewSingleton viewSingleton = ViewSingleton.getInstance();
 
     @FXML
     private Button front;
     @FXML
     private Button back;
 
-    public void setView(View view){
-        this.view = view;
+    public void initialize() {
+        StarterCard card = (StarterCard) viewSingleton.getView().getHand().getCard(0);
+        front.setGraphic(new CardBuilder(card).getCardImage());
+        card.flip();
+        back.setGraphic(new CardBuilder(card).getCardImage());
+        card.flip();
     }
 
     public void playStarterCard(MouseEvent mouseEvent) {
         try {
-            if(mouseEvent.getSource().equals(front)){
-                view.chooseCardSide(CardSide.FRONT);
+            if(mouseEvent.getSource().equals(front)) {
+                viewSingleton.getView().chooseCardSide(CardSide.FRONT);
                 return;
         }
-        view.chooseCardSide(CardSide.BACK);
+        viewSingleton.getView().chooseCardSide(CardSide.BACK);
         }
         catch (IOException | EmptyDeckException | GameDoesNotExistException | HandIsFullException | UnexistentUserException | WrongGamePhaseException e) {
         throw new RuntimeException(e);
