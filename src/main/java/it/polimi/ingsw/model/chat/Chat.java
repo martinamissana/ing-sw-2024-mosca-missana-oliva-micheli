687400000e0462ab716f8295b4797165704ca0a3
Giorgio <giorgio1.micheli@mail.polimi.it>
2024-06-25 16:56:06 +0200

    Chat Javadoc edits

--- src/main/java/it/polimi/ingsw/model/chat/Chat.java
+++ src/main/java/it/polimi/ingsw/model/chat/Chat.java
@@ -4,10 +4,12 @@ import it.polimi.ingsw.model.player.Player;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
- * Class Chat
- * each Player has an associated chat with a list of sent messages and a list of received messages
+ * Class Chat<br>
+ * each Player has an associated Chat, which contains separate lists for sent and received messages
  */
 public class Chat implements Serializable {
     private final ArrayList<Message> sentMessages;
@@ -22,162 +24,70 @@ public class Chat implements Serializable {
     }
 
     /**
-     * getter
-     *
-     * @return receivedMessages the list of received messages
+     * @return the player's list of sent messages
      */
-    public ArrayList<Message> getReceivedMessages() {
-        return receivedMessages;
-    }
+    public ArrayList<Message> getSentMessages() { return sentMessages; }
 
     /**
-     * getter
-     *
-     * @return sentMessages the list of sent messages
+     * @return the player's list of received messages
      */
-    public ArrayList<Message> getSentMessages() {
-        return sentMessages;
-    }
+    public ArrayList<Message> getReceivedMessages() { return receivedMessages; }
 
     /**
-     * getter
-     *
-     * @param sender the player that sent the messages
-     * @return list of the messages received from a specified player
-     */
-    private ArrayList<Message> getReceivedMessagesFromPlayer(Player sender) {
-        int i;
-        ArrayList<Message> list = new ArrayList<>();
-        for (i = 0; i < receivedMessages.size(); i++) {
-            if (!receivedMessages.get(i).isGlobal() && receivedMessages.get(i).getSender().equals(sender)) {
-                list.add(receivedMessages.get(i));
-            }
-        }
-        return list;
-    }
-
-    /**
-     * getter
-     *
-     * @param receiver the player that received the messages
-     * @return list of the messages sent to a specified player
-     */
-    private ArrayList<Message> getSentMessagesToPlayer(Player receiver) {
-        int i;
-        ArrayList<Message> list = new ArrayList<>();
-        for (i = 0; i < sentMessages.size(); i++) {
-            if (!sentMessages.get(i).isGlobal() && sentMessages.get(i).getReceiver().equals(receiver)) {
-                list.add(sentMessages.get(i));
-            }
-        }
-        return list;
-    }
-
-    /**
-     * getter
-     *
-     * @return list of global received message
-     */
-    private ArrayList<Message> getGlobalReceivedMessages() {
-        int i;
-        ArrayList<Message> list = new ArrayList<>();
-        for (i = 0; i < receivedMessages.size(); i++) {
-            if (receivedMessages.get(i).isGlobal()) {
-                list.add(receivedMessages.get(i));
-            }
-        }
-        return list;
-    }
-
-    /**
-     * getter
-     *
-     * @return list of global sent messages
-     */
-    private ArrayList<Message> getGlobalSentMessages() {
-        int i;
-        ArrayList<Message> list = new ArrayList<>();
-        for (i = 0; i < sentMessages.size(); i++) {
-            if (sentMessages.get(i).isGlobal()) {
-                list.add(sentMessages.get(i));
-            }
-        }
-        return list;
-    }
-
-    /**
-     * getter
-     *
-     * @return a list of the sent and received messages in the global chat in the order they have been sent
-     */
-    public ArrayList<Message> getGlobalChat() {
-        ArrayList<Message> sent = this.getGlobalSentMessages();
-        ArrayList<Message> received = this.getGlobalReceivedMessages();
-        ArrayList<Message> chat = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        while (i < sent.size() || j < received.size()) {
-            if (i >= sent.size()) {
-                while (j < received.size()) {
-                    chat.add(received.get(j));
-                    j++;
-                }
-                return chat;
-            }
-            if (j >= received.size()) {
-                while (i < sent.size()) {
-                    chat.add(sent.get(i));
-                    i++;
-                }
-                return chat;
-            }
-            if (sent.get(i).getOrder() <= received.get(j).getOrder()) {
-                chat.add(sent.get(i));
-                i++;
-            } else {
-                chat.add(received.get(j));
-                j++;
-            }
-        }
-        return chat;
-    }
-
-    /**
-     * getter
-     *
-     * @param player the player from which you want to get the private chat
-     * @return list of the sent and received messages in a private chat in the order they have been sent
+     * @param player any other player in the lobby
+     * @return a list of all private messages exchanged by this chat's player
+     * and the specified one, chronologically ordered (less to most recent)<br>
      */
     public ArrayList<Message> getPrivateChat(Player player) {
-        ArrayList<Message> sent = this.getSentMessagesToPlayer(player);
-        ArrayList<Message> received = this.getReceivedMessagesFromPlayer(player);
-        ArrayList<Message> chat = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        while (i < sent.size() || j < received.size()) {
-            if (i >= sent.size()) {
-                while (j < received.size()) {
-                    chat.add(received.get(j));
-                    j++;
-                }
-                return chat;
-            }
-            if (j >= received.size()) {
-                while (i < sent.size()) {
-                    chat.add(sent.get(i));
-                    i++;
-                }
-                return chat;
-            }
-            if (sent.get(i).getOrder() <= received.get(j).getOrder()) {
-                chat.add(sent.get(i));
-                i++;
-            } else if (sent.get(i).getOrder() > received.get(j).getOrder()) {
-                chat.add(received.get(j));
-                j++;
-            }
+        ArrayList<Message> res = this.getMessagesSentTo(player);
+        res.addAll(this.getMessagesReceivedFrom(player));
+        return (ArrayList<Message>) res.stream().sorted(Comparator.comparingInt(Message::getOrder)).toList();
+    }
 
-        }
-        return chat;
+    /**
+     * @param receiver any other player in the lobby
+     * @return a list of all private messages that the player sent to the specified player<br>
+     * returns an empty list if {@code receiver} is the same player this Chat is associated with
+     */
+    private ArrayList<Message> getMessagesSentTo(Player receiver) {
+        return (ArrayList<Message>) sentMessages.stream()
+                .filter(m -> !m.isGlobal() && // global messages aren't included
+                        m.getReceiver().equals(receiver))
+                .toList();
+    }
+
+    /**
+     * @param sender any other player in the lobby
+     * @return a list of all private messages that the player received from the specified player<br>
+     * returns an empty list if {@code sender} is the same player this Chat is associated with
+     */
+    private ArrayList<Message> getMessagesReceivedFrom(Player sender) {
+        return (ArrayList<Message>) receivedMessages.stream()
+                .filter(m -> !m.isGlobal() && // global messages aren't included
+                        m.getSender().equals(sender))
+                .toList();
+    }
+
+    /**
+     * @return a list of all global messages, chronologically ordered (less to most recent)
+     */
+    public ArrayList<Message> getGlobalChat() {
+        ArrayList<Message> res = this.getSentGlobalMessages();
+        res.addAll(this.getReceivedGlobalMessages());
+        return (ArrayList<Message>) res.stream().sorted(Comparator.comparingInt(Message::getOrder)).toList();
+    }
+
+    /**
+     * @return a list of all messages sent to the global chat by the player
+     */
+    private ArrayList<Message> getSentGlobalMessages() {
+        return (ArrayList<Message>) sentMessages.stream().filter(Message::isGlobal).toList();
+    }
+
+    /**
+     * @return a list of all messages received by the player from the global
+     */
+    private ArrayList<Message> getReceivedGlobalMessages() {
+        return (ArrayList<Message>) receivedMessages.stream().filter(Message::isGlobal).toList();
     }
 }
